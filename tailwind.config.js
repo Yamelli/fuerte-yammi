c50929d4f46063f5bb3be959711ff0ae8349c2bb
Yamelli <yamelli.hdez.h@gmail.com>
2025-08-17 00:52:25 -0600

    Ultima configuracion de Tailwind y App.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Esto es crucial: le dice a Tailwind dónde buscar clases
   ],
   theme: {
     extend: {},
